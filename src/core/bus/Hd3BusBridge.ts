171f7bb934559ae2ffa6e9193e668e191ee85e90
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2025-11-03 03:07:32 +0100

    + Improved bridge

--- src/core/bus/Hd3BusBridge.ts
+++ src/core/bus/Hd3BusBridge.ts
@@ -5,34 +5,50 @@ import { Hd3BusEndpoint } from './Hd3BusEndpoint';
 
 export interface Hd3BusBridgeOptions<Events extends Record<EventType, unknown>> {
   events?: (keyof Events)[];
-  buses: Hd3Bus<Events>[];
+  buses: Hd3Bus<Events>[][];
 };
 
+const __makeId = (() => {
+  let i = 0;
+  function __makeId(){
+    return i++;
+  }
+  return __makeId;
+})();
 
 /**
- * Bridges two buses to mutualise the events passed in the constructor (or all events if unspecified
+ * Bridges groups of buses by forwarding event from one group to all others, except buses in the same group.
  */
 export class Hd3BusBridge<Events extends Record<EventType, unknown> = Record<EventType, unknown>> {
   private events: (keyof Events)[] | undefined;
-  private endpoints: Hd3BusEndpoint<Events>[];
+  private endpoints: Hd3BusEndpoint<Events>[][];
+  private key: string;
   
-  private forwardFactory(bus: Hd3Bus<Events>){
-    return <T extends keyof Events>(
+  private forwardFactory(i: number){
+    return (<T extends keyof Events>(
       type:T,
       event:Events[T]
     ) => {
-      for(const endpoint of this.endpoints){
-        if(endpoint.bus !== bus) {
+      if((event as any)[this.key]) {
+        return;
+      }
+      (event as any)[this.key] = true;
+      for(let j=0 ; j<this.endpoints.length ; ++j){
+        if(j === i) {
+          continue;
+        }
+        for(const endpoint of this.endpoints[j]){
           endpoint.bus?.emit(type, event);
         }
       }
-    }
+    }).bind(this);
   }
 
   constructor(opts: Hd3BusBridgeOptions<Events>){
     this.events = opts.events;
-    this.endpoints = opts.buses.map((bus) => {
-      const forward = this.forwardFactory(bus);
+    this.key = '__forwarded-' + __makeId();
+    this.endpoints = opts.buses.map((buses, i) => {
+      const forward = this.forwardFactory(i);
       const listeners = (
         this.events === undefined
         ? {
@@ -42,15 +58,21 @@ export class Hd3BusBridge<Events extends Record<EventType, unknown> = Record<Eve
           return [eventType, (event: Events[T]) => forward(eventType, event)];
         }))
       )
-      return new Hd3BusEndpoint<Events>({
-        listeners: listeners as unknown as {[K in keyof Events]?: Handler<Events[K]>},
+      return buses.map((bus) => {
+        const res = new Hd3BusEndpoint<Events>({
+          listeners: listeners as unknown as {[K in keyof Events]?: Handler<Events[K]>},
+        });
+        res.bus = bus;
+        return res;
       });
     })
   }
 
   destroy(){
-    for(const endpoint of this.endpoints){
+    for(const endpoints of this.endpoints){
+      for(const endpoint of endpoints){
       endpoint.destroy();
+      }
     }
   }
   
